7192176fadc7f4879a3689c6ac21edbdabc81fe9
Phillipml <contato.phillip.menezes@gmail.com>
2026-06-17 09:35:31 -0300

    fix: tailwind config to fix hot reload

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,19 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  // NOTE: Update this to include the paths to all files that contain Nativewind classes.
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    colors:{
-      primary: "#E5E2E1",
-      secondary: "#D1C6AB",
-      gold:"#EDC200",
-      bgPrimary:"#1C1B1B",
-      bgSecondary:"#2A2A2A",
-      horde:"#8C1616",
-      alliance:"#004A94"
+    extend: {
+      colors: {
+        primary: "#E5E2E1",
+        secondary: "#D1C6AB",
+        gold: "#EDC200",
+        bgPrimary: "#1C1B1B",
+        bgSecondary: "#2A2A2A",
+        horde: "#8C1616",
+        alliance: "#004A94",
+      },
     },
-    extend: {},
   },
+  safelist: [
+    "text-gold",
+    "text-horde",
+    "text-alliance",
+    "text-primary",
+    "text-secondary",
+    "bg-bgPrimary",
+    "bg-bgSecondary",
+  ],
   plugins: [],
-}
+};
