21cc1c7f49d13398ce03783025930816919377bb
Phillipml <contato.phillip.menezes@gmail.com>
2026-06-23 15:41:55 -0300

    feat: functions to requisitions created

--- frontend/hooks/useRaces.ts
+++ frontend/hooks/useRaces.ts
@@ -1,6 +1,6 @@
 import { useQuery } from '@tanstack/react-query';
-import { fetchRaceClasses, fetchRaces } from '../services/api';
-import { Race, RaceClasses } from '../types/api';
+import { fetchClassSpecs, fetchRaceClasses, fetchRaces } from '../services/api';
+import { Race, RaceClasses, SpecList } from '../types/api';
 
 export function useRaces() {
   return useQuery<Race[]>({
@@ -14,5 +14,12 @@ export function useRaceClasses(race_id: number) {
     queryKey: ['raceClasses', race_id],
     queryFn: () => fetchRaceClasses(race_id),
     enabled: !!race_id,
+  });
+}
+export function useClassSpecs(race_id: string, class_id: string) {
+  return useQuery<SpecList>({
+    queryKey: ['classSpecs', race_id, class_id],
+    queryFn: () => fetchClassSpecs(race_id, class_id),
+    enabled: !!race_id && !!class_id,
   });
 }
--- frontend/services/api.ts
+++ frontend/services/api.ts
@@ -1,4 +1,4 @@
-import { Race, RaceClasses } from '@/types/api';
+import { Race, RaceClasses, SpecList } from '@/types/api';
 
 export async function fetchRaces(): Promise<Race[]> {
   const res = await fetch(`${process.env.EXPO_PUBLIC_API_URL}/playable-race/index?format=json`);
@@ -7,12 +7,21 @@ export async function fetchRaces(): Promise<Race[]> {
   }
   return res.json();
 }
-export async function fetchRaceClasses(race_id: number): Promise<RaceClasses> {
+export async function fetchRaceClasses(race_id: number | string): Promise<RaceClasses> {
   const res = await fetch(
     `${process.env.EXPO_PUBLIC_API_URL}/playable-race/${race_id}/playable-classes?format=json`,
   );
   if (!res.ok) {
     throw new Error('Failed to fetch race classes');
+  }
+  return res.json();
+}
+export async function fetchClassSpecs(race_id: string, class_id: string): Promise<SpecList> {
+  const res = await fetch(
+    `${process.env.EXPO_PUBLIC_API_URL}/playable-race/${race_id}/playable-classes/${class_id}/specs/?format=json`,
+  );
+  if (!res) {
+    throw new Error('Failed to fetch class specs');
   }
   return res.json();
 }
